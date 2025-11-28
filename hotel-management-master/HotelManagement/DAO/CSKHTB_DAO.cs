04b68f30df59d06ccb836101f171fdb1361a7465
31231021229@ueh.edu.vn <hanhly.31231021229@st.ueh.edu.vn>
2025-11-29 01:26:41 +0700

    aaa

---
+++ hotel-management-master/HotelManagement/DAO/CSKHTB_DAO.cs
@@ -0,0 +1,44 @@
+﻿using HotelManagement.DTO;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+public static class CSKH_ThongBao_DAO
+{
+    private static string connectionString =
+        ConfigurationManager.ConnectionStrings["HotelDTO"].ConnectionString;
+    // Đổi tên chuỗi kết nối theo app.config của m
+
+    public static List<ThongBaoCSKH> LayThongBaoHomNay()
+    {
+        List<ThongBaoCSKH> list = new List<ThongBaoCSKH>();
+
+        using (SqlConnection conn = new SqlConnection(connectionString))
+        {
+            conn.Open();
+            string sql = @"
+                SELECT MaTB, MaPH, NoiDung, ThoiGianGui
+                FROM CSKH_ThongBao
+                WHERE CONVERT(date, ThoiGianGui) = CONVERT(date, GETDATE())
+                ORDER BY ThoiGianGui DESC";
+
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            using (SqlDataReader rd = cmd.ExecuteReader())
+            {
+                while (rd.Read())
+                {
+                    list.Add(new ThongBaoCSKH
+                    {
+                        MaTB = rd["MaTB"].ToString(),
+                        MaPH = rd["MaPH"].ToString(),
+                        NoiDung = rd["NoiDung"].ToString(),
+                        ThoiGianGui = (DateTime)rd["ThoiGianGui"]
+                    });
+                }
+            }
+        }
+
+        return list;
+    }
+}
